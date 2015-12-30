b9496e30b764ea9d65daac27e0980340a15b876c
rotand <andrewrotary@gmail.com>
2015-12-30 20:30:00 +0200

    Basic Project for Developing a web api

--- src/main/java/com/paketi/AplicationInitializer.java
+++ src/main/java/com/paketi/AplicationInitializer.java
@@ -1,10 +1,14 @@
 package com.paketi;
 
+import com.paketi.Config.WebConfig;
 import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
 
 /**
  * Created by Root-World on 30.12.2015.
@@ -12,7 +16,14 @@ import javax.servlet.ServletException;
 
 public class AplicationInitializer implements WebApplicationInitializer {
 
+    private final static String DISPatcher = "dispatcher";
+
     public void onStartup(ServletContext servletContext) throws ServletException {
         AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+        ctx.register(WebConfig.class);
+        servletContext.addListener(new ContextLoaderListener(ctx));
+        ServletRegistration.Dynamic servlet = servletContext.addServlet("DISPATCHER", new DispatcherServlet(ctx));
+        servlet.addMapping("/");
+        servlet.setLoadOnStartup(1);
     }
 }
--- src/main/java/com/paketi/Controller/ReminderController.java
+++ src/main/java/com/paketi/Controller/ReminderController.java
@@ -1,18 +1,20 @@
 package com.paketi.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by Root-World on 30.12.2015.
  */
 @Controller
-@RequestMapping("/remind")
+@RequestMapping("/reminder")
 public class ReminderController {
-
+    @ResponseBody
     @RequestMapping(value = "/get", method = RequestMethod.GET)
-    public String getReminder(){
+    public String getReminder(ModelMap model){
         return "My Reminder";
     }
 }
